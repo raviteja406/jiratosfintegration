41877d1bab9fa36bf60652ff298c63287208f4f1
raviteja406 <raam1111@gmail.com>
2017-03-07 15:49:05 -0600

    Added JIRA update(PUT) code for an existing issue

--- post_custom_int.groovy
+++ post_custom_int.groovy
@@ -13,13 +13,15 @@ import org.apache.commons.codec.binary.Base64;
 import java.net.*;
 import groovy.json.JsonSlurper;
 import groovy.json.JsonBuilder;
+
 /*MS: Please remove redundant imports*/
+/*RT: Fixed: All the redundant fields are already removed as the code*/
 
 init();
 
 def init() {
-    if (!isIncidentCreated())
-        create_snow_incident();
+    //if (!isIncidentCreated())
+    sync_with_snow();
 }
 
 /* <<<These API signatures will change>>>
@@ -28,14 +30,23 @@ def init() {
 *							with the details of JIRA ticket.
 */
 
-def create_snow_incident() {
+def sync_with_snow() {
 
-    def user = "JIRA_API";                                                        //!< SNOW UserID
+    def user = "JIRA_API";                                                      //!< SNOW UserID
     def passwd = "5h1n30N4ever!";                                               //!< SNOW Password
-    def requestMethod = "POST";                                                 //!< Method Type
-    def URLParam = "https://sfsf.service-now.com/api/now/table/incident";       //!< SNOW Incident table URL
-    def query = "{\"short_description\":\"Test post request from JIRA INT\"}";  //!< Data to POST to SNOW
-    
+    def requestMethod
+    def URLParam = "https://sfsf.service-now.com/api/now/table/incident/"
+
+    if (!isIncidentCreated()) {
+        requestMethod = "POST";                                                 //!< Method Type
+    } else {
+        requestMethod = "PUT"
+        def customFieldManager = ComponentAccessor.getCustomFieldManager()
+        def cf_snow_sys_id = customFieldManager.getCustomFieldObject("customfield_18983");
+        URLParam = URLParam + issue.getCustomFieldValue(cf_snow_sys_id)
+    }
+    def query = "{\"short_description\":\"Test post request from JIRA INTEGRATION TEST\"}";  //!< Data to POST to SNOW
+
     /*MS: Please read all these Properties from a .Properties file. One suggestion is to use Groovy ConfigSlurper library*/
 
     URLConnection connection;                                                   //!< URL Connection object
@@ -59,7 +70,7 @@ def create_snow_incident() {
         connection.connect()
     } catch (Exception e) {
         //ToDo:
-        /*MS: Let's look at writing it to a log file. Jira installations use Log4J and we should be able to access 
+        /*MS: Let's look at writing it to a log file. Jira installations use Log4J and we should be able to access
             and utilize it for writing any failure conditions */
     }
     if (connection.getResponseCode() == 201) {
@@ -78,7 +89,7 @@ def create_snow_incident() {
 }
 
 /* <<<These API signatures will change>>>
-*	Description:	
+*	Description:
 *	setCustomFields(def result, def issue): This API is responsible for populating custom fields in JIRA
 *											once the ticket is created in SNOW and details are fetched from SNOW
 *	parameters:	[IN] result: This will be JSON response from SNOW
@@ -93,16 +104,20 @@ def setCustomFields(def result, def issue) {
     def snow_etask_id = "customfield_18982";
     def snow_sys_id = "customfield_18983";
     def snow_url = "customfield_18984";
-    
-    /*MS: I confirmed that following deep-link URL does work: 
+
+    /*MS: I confirmed that following deep-link URL does work:
     https://sfsf.service-now.com/incident.do?sys_id=f629752adbb076408e5ef9231d961978
-    Concatenate: def SNIncidentURL = URL + 'incident.do?sys_id= + Sys_Id) 
-    Sys_Id returned from SN in above format and SET(SNIncidentURL ) to a custom URL field in JIRA. 
+    Concatenate: def SNIncidentURL = URL + 'incident.do?sys_id= + Sys_Id)
+    Sys_Id returned from SN in above format and SET(SNIncidentURL ) to a custom URL field in JIRA.
     If User is already logged in the click takes him/her to the Incident detail page else to the login page.
     */
-    
-    def snow_test_url = "https://sfsf.service-now.com/nav_to.do?uri=%2Fincident.do%3Fsys_id%3D838a52f6db4d3e408e5ef9231d9619cc%26sysparm_stack%3D%26sysparm_view%3D";
-    
+    /*RT: Fixed*/
+
+    def snow_base_url = "https://sfsf.service-now.com/"
+    def incident_key = "incident.do?sys_id="
+    def sys_id = result.result.sys_id;
+    def snow_inc_url = "$snow_base_url$incident_key$sys_id".toString()
+
     /*MS: Please read all these Properties from a .Properties file. One suggestion is to use Groovy ConfigSlurper library*/
 
     def customFieldManager = ComponentAccessor.getCustomFieldManager()
@@ -115,11 +130,11 @@ def setCustomFields(def result, def issue) {
     cf_snow_inc_id.updateValue(null, issue, new ModifiedValue(issue.getCustomFieldValue(cf_snow_inc_id), result.result.number), changeHolder);
     cf_snow_etask_id.updateValue(null, issue, new ModifiedValue(issue.getCustomFieldValue(cf_snow_etask_id), "ETASK123"), changeHolder);
     cf_snow_sys_id.updateValue(null, issue, new ModifiedValue(issue.getCustomFieldValue(cf_snow_sys_id), result.result.sys_id), changeHolder);
-    cf_snow_url.updateValue(null, issue, new ModifiedValue(issue.getCustomFieldValue(cf_snow_url), snow_test_url), changeHolder);
+    cf_snow_url.updateValue(null, issue, new ModifiedValue(issue.getCustomFieldValue(cf_snow_url), snow_inc_url), changeHolder);
 }
 
 /* <<<These API signatures will change>>>
-*	Description:	
+*	Description:
 *	setCustomFields(def result, def issue): This API is responsible for populating custom fields in JIRA
 *											once the ticket is created in SNOW and details are fetched from SNOW
 *	parameters:	[IN] result: This will be JSON response from SNOW
