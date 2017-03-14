abf16d9b838a3f46805fe522b72fbcc94dffaa9c
raviteja406 <raam1111@gmail.com>
2017-03-14 16:22:48 -0500

    Added code with attachments and logs-latest file-sendattachments_with_log_latest_code.groovy

--- sendattachments_with_log_latest_code.groovy
+++ sendattachments_with_log_latest_code.groovy
@@ -3,7 +3,6 @@ import com.atlassian.jira.ComponentManager
 import com.atlassian.jira.issue.fields.CustomField;
 import com.atlassian.jira.issue.CustomFieldManager;
 import com.atlassian.jira.issue.Issue
-import com.atlassian.jira.issue.fields.CustomField
 import com.atlassian.jira.issue.ModifiedValue
 import com.atlassian.jira.issue.MutableIssue
 import com.atlassian.jira.issue.util.DefaultIssueChangeHolder
@@ -15,22 +14,15 @@ import groovy.json.JsonSlurper;
 import groovy.json.JsonBuilder;
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
+import org.apache.log4j.Category
+import org.apache.commons.io.IOUtils;
+import com.atlassian.jira.util.io.InputStreamConsumer;
+import com.atlassian.jira.util.PathUtils
+import java.nio.charset.StandardCharsets
 
 init();
 
 def init() {
-	
-    sync_with_snow();
-
-}
-
-/* <<<These API signatures will change>>>
-*	create_snow_incident(): This API is responsible for establishing connection
-*							between JIRA and SNOW and create an incident in SNOW
-*							with the details of JIRA ticket.
-*/
-
-def sync_with_snow() {
 
     //Reading from properties file
     Properties props = new Properties()
@@ -39,6 +31,16 @@ def sync_with_snow() {
 
     def log = Logger.getLogger(props.getProperty('LOG_FILE_PATH'))
     log.setLevel(Level.ERROR)
+    sync_with_snow(props)
+}
+
+/* <<<These API signatures will change>>>
+*	create_snow_incident(): This API is responsible for establishing connection
+*							between JIRA and SNOW and create an incident in SNOW
+*							with the details of JIRA ticket.
+*/
+
+def sync_with_snow(def props) {
 
     def requestMethod
     def URLParam = props.getProperty('incidenturlparam');
@@ -78,8 +80,7 @@ def sync_with_snow() {
     } catch (Exception e) {
         log.error("JIRA-SNOW: ERROR: Unable to connect:" + e)
     }
-    if ((connection.getResponseCode().toString() == props.getProperty('CREATED')) ||
-        (connection.getResponseCode().toString() == props.getProperty('OK'))) {
+    if ((connection.getResponseCode().toString() == props.getProperty('CREATED'))) {
 
         String nextLine;
         InputStreamReader inStream = new InputStreamReader(connection.getInputStream());
@@ -88,10 +89,13 @@ def sync_with_snow() {
         // Extracting details of SNOW response
         def slurper = new JsonSlurper()
         def result = slurper.parseText(buff.readLine())
+
+        sendAttachments(props, result)
         setCustomFields(result, issue, props)
-        //return connection.getResponseMessage(); /*MS: The calling function should do something with this return*/
+        //return connection.getResponseCode().toString()
     } else {
         log.error("JIRA-SNOW: ERROR: Response:" + connection.getResponseMessage())
+        //return connection.getResponseCode().toString()
     }
 }
 
@@ -128,6 +132,7 @@ def setCustomFields(def result, def issue, def props) {
     cf_snow_etask_id.updateValue(null, issue, new ModifiedValue(issue.getCustomFieldValue(cf_snow_etask_id), "ETASK123"), changeHolder);
     cf_snow_sys_id.updateValue(null, issue, new ModifiedValue(issue.getCustomFieldValue(cf_snow_sys_id), result.result.sys_id), changeHolder);
     cf_snow_url.updateValue(null, issue, new ModifiedValue(issue.getCustomFieldValue(cf_snow_url), snow_inc_url), changeHolder);
+
 }
 
 /* <<<These API signatures will change>>>
@@ -159,5 +164,81 @@ def isIncidentCreated() {
         return true;
     } else {
         return false;
+    }
+}
+
+def sendAttachments(def props, def response) {
+
+    def passwd = props.getProperty('passwd');
+    def user = props.getProperty('user');
+    log.error "sending attachments"
+    // Get the current issue key
+    Issue issueKey = issue
+    def id = issueKey.getId()
+
+    // Get a manager to handle the copying of attachments
+    def attachmentManager = ComponentAccessor.getAttachmentManager()
+
+    // Get the default attachment path from its manager
+    def attachmentPathManager = ComponentAccessor.getAttachmentPathManager().attachmentPath.toString()
+    def attachments = attachmentManager.getAttachments(issueKey) //attachments is list<attachments>
+
+    FileInputStream fis;
+    int size = 0;
+    byte[] buffer;
+    def encodeddata; //byte array
+    def str_encodeddata; //string
+    DataOutputStream wr;
+
+    for (int i = 0; i < attachments.size(); i++) {
+        String filePath = PathUtils.joinPaths(ComponentAccessor.getAttachmentPathManager().getDefaultAttachmentPath(), issueKey.getProjectObject().getKey(), issue.getKey(), attachments[i].getId().toString());
+        File file = new File(filePath);
+
+        if (file.exists()) {
+            try {
+                URL url;
+                URLParam = props.getProperty('attachmenturlparam')
+                URLParam = URLParam + response.result.sys_id + "&file_name=" + attachments[i].getFilename()
+
+                url = new URL(URLParam);
+                def authString = user + ":" + passwd;
+                byte[] authEncBytes = Base64.encodeBase64(authString.getBytes());
+                String authStringEnc = new String(authEncBytes);
+                connection = url.openConnection();
+                connection.setRequestProperty("Authorization", "Basic " + authStringEnc);
+                connection.setRequestMethod("POST");
+                connection.setDoOutput(true);
+                connection.setDoInput(true);
+
+                def mimeType = connection.guessContentTypeFromName(attachments[i].getFilename());
+
+                connection.setRequestProperty("Content-Type", mimeType);
+                connection.setRequestProperty("Accept", "application/json");
+                connection.setRequestProperty("Content-Length", Integer.toString(str_encodeddata.toString().getBytes().length));
+
+                buffer = new byte[file.length()];
+                fis = new FileInputStream(file);
+
+                int read;
+                while ((read = fis.read(buffer)) != -1) {
+                    connection.getOutputStream().write(buffer, 0, read);
+                }
+
+                encodeddata = Base64.encodeBase64(buffer);
+                str_encodeddata = new String(buffer);
+                fis.close();
+
+                wr = new DataOutputStream(connection.getOutputStream());
+                wr.writeBytes(str_encodeddata);
+                wr.flush();
+                wr.close();
+                connection.connect();
+            } catch (Exception e) {
+                log.error "Exception while send" + e
+            }
+        }
+        if (connection.getResponseCode() != props.getProperty('CREATED')) {
+            log.error "Error response from server for attachment" + connection.getResponseMessage()
+        }
     }
 }
